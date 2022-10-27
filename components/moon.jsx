235b82b97acda8aff9e2d77a0d8b85eec425fc72
lisa-von-mond <lisalue@posteo.de>
2022-10-27 16:19:55 +0200

    code moon function

--- components/moon.jsx
+++ components/moon.jsx
@@ -3,26 +3,59 @@ import styled, {css} from 'styled-components'
 import {mooncalendar} from '../utils/moon-calendar.js'
 import Image from 'next/image'
 import moon from '../public/moon_mask_white.png'
+import { useState, useEffect } from 'react'
 
 export function Moon({color}){
 
+const [moonPhase, setMoonPhase] = useState(null)
+const [nextFullMoon, setNextFullMoon] = useState(null)
+
 const today = new Date()
 
-const moonMonth = today.getMonth() + 1;   
-const moonYear = today.getFullYear(); 
-const moonDate = today.getDate();
+function transformDate(d){
 
-const todayDate = moonDate.toString() + "/" + moonMonth.toString() + "/" + moonYear.toString()
-console.log(today)
+const moonMonth = d.getMonth() + 1;   
+const moonYear = d.getFullYear(); 
+const moonDate = d.getDate();
+return moonDate.toString() + "/" + moonMonth.toString() + "/" + moonYear.toString()
+}
+
+const todayDate = transformDate(today);
+
+const moonReadable = mooncalendar.map(e => ({...e, milliseconds: (new Date(e.date)).getTime()}))
+
+useEffect(() => {
+const currentms = today.getTime()
+const filtered = moonReadable.filter(e => e.milliseconds > currentms)
+
+function generatePhase()
+  {if (filtered[0].state === "full moon" || filtered[1].state === "full moon" )
+    {setMoonPhase("increasing moon")}
+  else
+    {setMoonPhase("waning moon")}}
+  
+generatePhase()
+
+function generateNextFullMoon(){
+  const next = (filtered.filter(e => e.state = "full moon"))[0]
+  setNextFullMoon(transformDate(new Date(next.date)))
+}
+
+generateNextFullMoon()
+
+}
+
+)
+
 
 return(
   <MoonWrapper color={color}>
     <div className="moonpic"><Image src={moon} width="70" height="70"></Image></div>
   
   <div><p>{todayDate}</p></div>
-  <div><p className="spacer">  </p><p>increasing o</p></div>
+  <div><p className="spacer">  </p><p>{moonPhase}</p></div>
   <div><p>next full moon:</p></div>
-  <div><p className="spacer">   </p><p>09/11/2022</p></div>
+  <div><p className="spacer">   </p><p>{nextFullMoon}</p></div>
   </MoonWrapper>
 )    
 }
